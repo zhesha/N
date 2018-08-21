c88e514118cf1cb693b46ab6daca99a079c07c03
zhesha <savzhe@gmail.com>
2018-08-21 11:40:37 +0300

    builds

--- Unity/Nitrogenium/Assets/Resources/Scripts/Controllers/MenuController.cs
+++ Unity/Nitrogenium/Assets/Resources/Scripts/Controllers/MenuController.cs
@@ -8,4 +8,8 @@ public class MenuController: MonoBehaviour {
     public void startGame () {
         SceneManager.LoadScene("GameScene");
     }
+
+    public void toExit () {
+        Application.Quit();
+    }
 }
